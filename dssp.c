b24742f9be8c99f61a407fc11f19934b24a0efa8
beadleha <beadleha@clarkson.edu>
2016-02-07 03:06:30 -0500

    Correct parsing for numbers to deal with 2-, 3-, etc

--- dssp.c
+++ dssp.c
@@ -19,6 +19,27 @@ void showStack(elem * stack){
 	printf("\n");
 }
 
+// This can be reduced but it hits every case
+int isnum(char * foo){
+	int i=0;
+	char temp = foo[0];
+
+	if(foo == NULL) return 0;
+	if(strlen(foo) == 0) return 0;
+
+	// If first is minus, scan from beginning to see if all are digits
+	if(temp=='-'){
+		for (i=1; i < (strlen(foo)); i++){
+			printf("check %d\n",i);
+			if(!isdigit(foo[i])) return 0;
+		}
+	}else for (i=0; i < (strlen(foo)); i++){
+		printf("check %d\n",i);
+		if(!isdigit(foo[i])) return 0;
+	}
+	return 1;
+}
+
 elem * getSequence(){
 	char ch;
 	int i=0;
@@ -36,6 +57,7 @@ elem * getSequence(){
 		if (ch == '['){
 			while(((ch = getchar()) != ']'));
 		} else if(ch != ' '){
+			if(i>8) break;
 			seqtail->chars[i++] = ch;
 		} else if (i != 0){ // Handles adjacent spaces
 			seqtail->chars[i] =  '\0';
@@ -81,11 +103,10 @@ int main(){
 	
 		do{
 			assert(tempSeq != NULL);
-			if(isdigit(tempSeq->chars[0])){
+			if(isnum(tempSeq->chars)){
 				tempStack = stack;
 				stack = malloc(sizeof(elem));
 				stack->next = tempStack;
-
 				stack->value = atoi(tempSeq->chars);
 			}else{
 				stack = wordFind(tempSeq->chars, stack, vocab);
