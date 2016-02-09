872f280d69c8fb58bf80add03ddb069c0a6b6ad6
beadleha <beadleha@clarkson.edu>
2016-02-09 17:52:44 +0000

    Added files that I forgot to add earlier

---
+++ stack.c
@@ -0,0 +1,43 @@
+/*	This file is part of libreDSSP.
+
+	Copyright 2016 Alan Beadle
+
+	libreDSSP is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	libreDSSP is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with libreDSSP.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include <malloc.h>
+#include "stack.h"
+
+stack * newStack(){
+	stack * stack = malloc(sizeof(stack));
+	stack->array = malloc(10*sizeof(int)); // TODO Make dynamic
+	stack->size = 10;
+	stack->top = -1; // -1 indicates empty stack
+	return stack;
+}
+
+// If stack is empty, do not use!
+int top(stack * stack) {
+	return (stack->array[stack->top]);
+}
+
+// If stack is empty, do not use!
+int pop(stack * stack) {
+	return (stack->array[(stack->top)--]);
+}
+
+void push(stack * stack, int value) {
+	(stack->top)++;
+	stack->array[stack->top] = value;
+	return;
+}
